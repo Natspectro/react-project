9332e87292f69145349f407d6c2ac9460b84dc50
Nathan <natspectro@gmail.com>
2023-02-18 23:34:59 +0100

    Added reactive state to Home.js.

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
-  const handleClick = (e) => {
-    console.log("Hello, dev", e);
+  // let name = "Shirahoshi";
+  const [name, setName] = useState("Nathan");
+  const [age, setAge] = useState(32);
+  const handleClick = () => {
+    setName("Janice");
+    setAge(30);
   };
 
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>
+        {name} is {age} years old
+      </p>
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
