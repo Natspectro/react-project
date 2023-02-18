f3f393717292ff1aede4604623075aa7f2127244
Nathan <natspectro@gmail.com>
2023-02-18 22:30:11 +0100

    Added CSS styles, Removed App.css, Edited Home.js and App.js.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import "./App.css";
 import Navbar from "./Navbar";
 import Home from "./Home";
 
 function App() {
-  const title = "Welcome to my blog";
-  const likes = 20;
-  const link = "http://www.google.com";
-
   return (
     <div className="App">
       <Navbar />
